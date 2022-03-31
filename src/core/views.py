4f30a9c5db0da872ff44abc76867f94645da4590
aadilkadiwal <aadilkadiwal4277@gmail.com>
2022-03-31 12:54:01 +0530

    Added Sign Up

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,27 @@
 from importlib.metadata import requires
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def home(request):
     
-    return render(request, 'core/home.html')
+    count = User.objects.count()
+
+    return render(request, 'core/home.html',{
+        'count': count
+    })
+
+def signup(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('home')
+    else:
+        form = UserCreationForm()    
+
+    return render(request, 'registration/signup.html',{
+        'form': form
+    }
+    )
